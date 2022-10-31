5b0772ba2671f0e74cf86484ed8cd1a054008683
tranph21 <tranph21@sps-prosek.cz>
2022-10-31 11:14:21 +0100

    change from home

--- main.py
+++ main.py
@@ -12,5 +12,27 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+#
+# another important thing is how git works
+# you turn on pc, download the changes, you make a change to the code
+# so you change the code and approve it
+# you would have to return to the last version of the fucking G
+#
+# if you would make 10000000 changes, then the programe would have to make 1 000 000 ways back
+#
+# git copys the files and causes data redundance. Here you can see the process of workflow,
+# there are 3 areaas you store your files: working directory, staging area, git directory (repository)
+#
+# you in your working directory make a change and the chagnge will stage ( for example I change file A and then file ABCDEF)
+# you approove the changes by "COMMITING"
+#
+# Modify, Stage, Commit
+# untucked file, you have to add, remove, or ignore new files (let's say db.php, login credentials etc.')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# You would leak your password, that's why you add it to git ignore, otherwise your passwords will be leaked.
+# Every file has it's unicate commit. check sum - it's for keeping the same versions, so people know that it's the same
+
+# you basically make a hash of your version and then people compare the hash of the received file with your hash, so they
+# can check if it's legitimate
+
+# najit si jak udelat commit
